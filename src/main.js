2b65865b8ffd14c9e1c5b9ad3edf7c1489bba682
HGKcoding <huguokang2017@gmail.com>
2018-01-18 11:43:59 +0800

    修改了项目结构，增加了基础的样式文件

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+// import router from './router'
+import fastclick from 'fastclick'
+import 'common/stylus/index.styl'
 
+fastclick.attach(document.body)// 解決移動端三百毫秒延遲
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
+  // router,
   components: { App },
   template: '<App/>'
 })
